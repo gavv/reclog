737cf921c1947c35f06bb5e2fd2495f588d01f5c
Victor Gaydov <victor@enise.org>
2025-05-16 03:40:45 +0900

    Check CHANGES.md in check_release.sh

--- script/check_release.sh
+++ script/check_release.sh
@@ -5,19 +5,23 @@ set -euo pipefail
 cd "`dirname "$0"`/.."
 
 git_version=`git describe --tags HEAD | sed -e 's/^v//'`
-toml_version=`sed -n 's/^version\s*=\s*"\(.*\)"/\1/p' Cargo.toml | head -1`
-rst_version=`sed -n 's/^:Footer: reclog \(.*\)/\1/p' MANUAL.rst`
-troff_version=`sed -n 's/.TH .* "reclog \(.*\)"/\1/p' reclog.1`
+cargo_toml_version=`sed -n 's/^version\s*=\s*"\(.*\)"/\1/p' Cargo.toml | head -1`
+changes_md_version=`sed -n 's/^##\s*\[v\([0-9.]*\)\].*/\1/p' CHANGES.md | head -1`
+man_rst_version=`sed -n 's/^:Footer: reclog \(.*\)/\1/p' MANUAL.rst`
+man_troff_version=`sed -n 's/.TH .* "reclog \(.*\)"/\1/p' reclog.1`
 
 echo "Detected versions:"
 echo "  Git tag:     $git_version"
-echo "  Cargo.toml:  $toml_version"
-echo "  MANUAL.rst:  $rst_version"
-echo "  reclog.1:    $troff_version"
+echo "  Cargo.toml:  $cargo_toml_version"
+echo "  CHANGES.md:  $changes_md_version"
+echo "  MANUAL.rst:  $man_rst_version"
+echo "  reclog.1:    $man_troff_version"
 
 if [ "${1:-}" != "-n" ]; then
-  if [[ "$git_version" != "$toml_version" || "$git_version" != "$rst_version" \
-          || "$git_version" != "$troff_version" ]]; then
+  if [[ "$git_version" != "$cargo_toml_version" \
+          || "$git_version" != "$changes_md_version" \
+          || "$git_version" != "$man_rst_version" \
+          || "$git_version" != "$man_troff_version" ]]; then
     echo
     echo "Version mismatch detected!"
     exit 1
